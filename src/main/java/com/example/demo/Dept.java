74a92497bc00c48681003ae3332329d4b42e9efa
keertiAllada <alladakeerti@gmail.com>
2021-09-22 10:38:48 +0530

    added dept

---
+++ src/main/java/com/example/demo/Dept.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+@Data
+public class Dept {
+
+    private int deptId;
+    private String  deptName;
+    private int strength;
+
+
+
+}
+
